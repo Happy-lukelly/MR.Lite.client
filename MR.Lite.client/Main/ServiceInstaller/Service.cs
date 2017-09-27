24642cc4d589840f24a34f7e2cee3cde2f2d0c8f
yanyulong <yulongyan1994@live.com>
2017-09-27 18:29:17 +0800

    删除了RegistryKeyInfo.cs 类文件 ,改RegistryKeyInfo类为Service类的内部类

--- MR.Lite.client/Main/ServiceInstaller/Service.cs
+++ MR.Lite.client/Main/ServiceInstaller/Service.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace Main.ServiceInstaller
 {
@@ -72,6 +73,25 @@ namespace Main.ServiceInstaller
             this.AnotherKeys = new List<RegistryKeyInfo>();
         }
         #endregion
+
+        /// <summary>
+        /// 抽象的一个代表注册表键的值的Model
+        /// </summary>
+        public class RegistryKeyInfo
+        {
+            /// <summary>
+            /// 键对应的值
+            /// </summary>
+            public object Value { get; set; }
+            /// <summary>
+            /// 键名
+            /// </summary>
+            public string Name { get; set; }
+            /// <summary>
+            /// 值类型
+            /// </summary>
+            public RegistryValueKind KeyKind { get; set; }
+        }
     }
 
     /// <summary>
--- MR.Lite.client/UnitTest/UnitTest1.cs
+++ MR.Lite.client/UnitTest/UnitTest1.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Main.ServiceInstaller;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace UnitTest
 {
+    public delegate void TestDele();
     [TestClass]
     public class UnitTest1
     {
@@ -12,6 +15,72 @@ namespace UnitTest
         {
             ServiceHelper helper = new ServiceHelper();
             helper.GetService("test");
+        }
+        [TestMethod]
+        public void Test()
+        {
+            TestClass test = new TestClass();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                test.TestMethod();
+            }
+            sw.Stop();
+            long R1 = sw.ElapsedMilliseconds;
+
+
+
+            sw.Start();
+            Type type2 = typeof(TestClass);
+            MethodInfo method2 = type2.GetMethod("TestMethod");
+            for (int i = 0; i < 1000000; i++)
+            {
+                method2.Invoke(test, null);
+            }
+            sw.Stop();
+
+            long R2 = sw.ElapsedMilliseconds;
+
+
+            Action action2 = System.Delegate.CreateDelegate(typeof(Action),test, method2) as Action;
+            sw.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                action2();
+            }
+            sw.Stop();
+            long R5 = sw.ElapsedMilliseconds;
+
+            Action action = new Action(test.TestMethod);
+            sw.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                action();
+            }
+            sw.Stop();
+            long R4 = sw.ElapsedMilliseconds;
+
+            Debug.Print(R4.ToString());
+
+            sw.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                Type type = typeof(TestClass);
+                MethodInfo method = type.GetMethod("TestMethod");
+                method.Invoke(test, null);
+            }
+            sw.Stop();
+
+            long R3 = sw.ElapsedMilliseconds;
+        }
+    }
+
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+
         }
     }
 }
